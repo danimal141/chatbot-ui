4ab961efe0dd9c0fe76272a921c6fda7d354ad21
danimal141 <hideaki.ishii1204@gmail.com>
2023-04-03 10:59:30 +0900

    Remove unused var

--- pages/index.tsx
+++ pages/index.tsx
@@ -496,8 +496,6 @@ const Home: React.FC<HomeProps> = ({
   // PROMPT OPERATIONS --------------------------------------------
 
   const handleCreatePrompt = () => {
-    const lastPrompt = prompts[prompts.length - 1];
-
     const newPrompt: Prompt = {
       id: uuidv4(),
       name: `Prompt ${prompts.length + 1}`,
