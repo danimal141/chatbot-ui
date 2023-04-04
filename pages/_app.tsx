362b636b84015f9c9b9cdf3bd8dc3d353166229e
danimal141 <hideaki.ishii1204@gmail.com>
2023-04-04 12:14:03 +0900

    Format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,20 +15,20 @@ function App({ Component, pageProps }: AppProps<{}>) {
   };
 
   return (
-  <Auth0Provider
-    domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"] || ""}
-    clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"] || ""}
-    authorizationParams={{
-      redirect_uri:
-        typeof window !== 'undefined' ? window.location.origin : undefined,
-    }}
-    onRedirectCallback={onRedirectCallback}
-  >
-    <div className={inter.className}>
-      <Toaster />
-      <Component {...pageProps} />
-    </div>
-  </Auth0Provider>
+    <Auth0Provider
+      domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"] || ""}
+      clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"] || ""}
+      authorizationParams={{
+        redirect_uri:
+          typeof window !== 'undefined' ? window.location.origin : undefined,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <div className={inter.className}>
+        <Toaster />
+        <Component {...pageProps} />
+      </div>
+    </Auth0Provider>
   );
 }
 
